17e529c1e20d120e042b9421c219d16ac5c8d0b1
Mary <rainofdune@gmail.com>
2024-12-22 21:36:35 +0500

    geoquiz1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.geoquiz"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
